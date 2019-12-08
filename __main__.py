c9fa58f7213fffca20f8e5da8ae0e75643c36700
Leonardo Florentino <leonardo.marcao@unesp.br>
2019-12-07 22:04:18 -0300

    On class APP add call to open weather api and mount df for this #3

--- __main__.py
+++ __main__.py
@@ -1,3 +1,6 @@
 from weather_mini_challenge.app import App
+
+
 if __name__ == '__main__':
     app = App()
+    app
--- weather_mini_challenge/app.py
+++ weather_mini_challenge/app.py
@@ -1,9 +1,14 @@
 from weather_mini_challenge.modules.open_weather_api import OpenWeatherAPI
 from weather_mini_challenge.config.base import CITY_NAME, COUNTRY_ID
+import pandas as pd
 
 
 class App(object):
     def __init__(self):
-        open_weather_api = OpenWeatherAPI(city_name=CITY_NAME,
-                                          country_id=COUNTRY_ID)
+        self.open_weather_api = OpenWeatherAPI(city_name=CITY_NAME,
+                                               country_id=COUNTRY_ID)
+        self.df = pd.DataFrame(self.open_weather_api.forecast_by_name())
+        
+
+
 
